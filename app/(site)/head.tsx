927d8441e074d1ab3901c1e316edecf3a910fdfe
ancweb <anccouncil.web@gmail.com>
2026-05-16 22:38:03 +0530

    Update head.tsx

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -3,6 +3,7 @@ export default function Head() {
     <>
       <title> Solid | Next.js Template for Startup and SaaS</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="google-site-verification" content="pXhPc7pZ99SO53K8YiQeaL6IRPYAAg18wZxNZdYickE" />
       <meta name="description" content="Built with Next.js and TypeScript" />
       <link rel="icon" href="/images/favicon.ico" />
     </>
