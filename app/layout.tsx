85c4b261d9798fd0fb7db3c071f184a65292ba5d
Priya Panwar <you@example.com>
2026-05-19 16:22:43 +0530

    added seo metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import "./globals.css";
 
 export const metadata = {
+  title: "AnC Council IIT Kanpur",
+  description: "Official website of AnC Council IIT Kanpur",
   verification: {
     google: "cDZiuecVhr0neGXyQwLYAYkv_2zT5CfCiVJHkmnnFuU",
   },
