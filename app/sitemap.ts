967283ecf3cec74029b3c391c67ba45924f7cf55
Priya Panwar <you@example.com>
2026-05-19 15:38:15 +0530

    added sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://anc-website-blond.vercel.app",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://anc-website-blond.vercel.app/team",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://anc-website-blond.vercel.app/blog",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://anc-website-blond.vercel.app/support",
+      lastModified: new Date(),
+    },
+  ];
+}
